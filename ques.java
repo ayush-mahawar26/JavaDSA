38232634dd880ee0a1fc3b4115e7a182b993f502
ayush <ayushmahawar60@gmail.com>
2023-02-26 03:28:34 +0530

    dsa

--- ques.java
+++ ques.java
@@ -1,15 +1,27 @@
-
 import java.util.*;
 
 public class ques {
+    static int minimum(int[] arr) {
+        Map<Integer, Integer> ans = new HashMap<>();
 
-    
+        for (int i : arr) {
+            ans.put(i, ans.getOrDefault(i, 0) + 1);
+        }
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> i : ans.entrySet()) {
+            min = Math.min(i.getValue(), min);
+        }
+
+        return min;
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = {1,2} ;
+        int[] arr = { 2, 3, 1, 2, 2, 2, 3, 2, 2, 3, 1 };
+
+        System.out.println(minimum(arr));
 
         sc.close();
 
