9cda3e2727bf3c357b99aba7fabe7b25d6c0a931
ayush <ayushmahawar60@gmail.com>
2023-02-26 14:18:54 +0530

    uber ques

--- ques.java
+++ ques.java
@@ -1,27 +1,60 @@
 import java.util.*;
 
 public class ques {
-    static int minimum(int[] arr) {
-        Map<Integer, Integer> ans = new HashMap<>();
 
-        for (int i : arr) {
-            ans.put(i, ans.getOrDefault(i, 0) + 1);
-        }
-        int min = Integer.MAX_VALUE;
-        for (Map.Entry<Integer, Integer> i : ans.entrySet()) {
-            min = Math.min(i.getValue(), min);
+    static boolean isPrime(int num) {
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
         }
 
-        return min;
+        if (!flag)
+            return true;
+        return false;
+    }
+
+    static boolean isArraySorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= arr[i - 1])
+                return false;
+        }
+
+        return true;
+    }
+
+    static boolean isPossible(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int ele = arr[i];
+            for (int j = ele; j > 0; j--) {
+                if (isPrime(j)) {
+                    if (i == 0) {
+                        ele -= j;
+                        arr[i] = ele;
+                        break;
+                    } else {
+                        if (ele - j > arr[i - 1]) {
+                            ele -= j;
+                            arr[i] = ele;
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return isArraySorted(arr);
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = { 2, 3, 1, 2, 2, 2, 3, 2, 2, 3, 1 };
-
-        System.out.println(minimum(arr));
+        int[] arr = { 10, 9, 13, 9 };
+        System.out.println(isPossible(arr));
 
         sc.close();
 
